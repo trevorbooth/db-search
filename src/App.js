a0a6d10ed1618081d516aa79acd84eac8d260655
Trevor <trevor@OTI-BigMac1.local>
2019-12-17 20:53:31 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello Trevor'
+    };
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.string}
         </p>
+        <button onClick={() => this.setState({ string: 'Hello World' })}>Change Text</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,5 +32,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
